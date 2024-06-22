3497a83c8e6e31da9a0d6455d7b320843a566c6f
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2024-06-22 15:10:19 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+setup(
+    name='CrossSiameseNet',
+    version='0.1.0',
+    packages=find_packages(include=['CrossSiameseNet', 'CrossSiameseNet.*'])
+)
