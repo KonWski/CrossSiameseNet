56e8262c71783a24a7b8bb574a24d65ba9719722
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2024-07-09 18:27:43 +0200

    csn train

--- CrossSiameseNet/CrossSiameseNet.py
+++ CrossSiameseNet/CrossSiameseNet.py
@@ -19,14 +19,14 @@ class CrossSiameseNet(nn.Module):
         self.n_models = len(models)
         self.cf_size = models[0].cf_size
         self.features = nn.Sequential(
-            self.linear_1 = nn.Linear(self.n_models*2*self.cf_size, 2*self.cf_size)
-            self.batch_norm_1 = nn.BatchNorm1d(2*self.cf_size)
+            nn.Linear(self.n_models*2*self.cf_size, 2*self.cf_size),
+            nn.BatchNorm1d(2*self.cf_size),
 
-            self.linear_2 = nn.Linear(2*self.cf_size, 2*self.cf_size)
-            self.batch_norm_2 = nn.BatchNorm1d(2*self.cf_size)
+            nn.Linear(2*self.cf_size, 2*self.cf_size),
+            nn.BatchNorm1d(2*self.cf_size),
 
-            self.linear_3 = nn.Linear(2*self.cf_size, self.cf_size)
-            self.batch_norm_3 = nn.BatchNorm1d(self.cf_size)
+            nn.Linear(2*self.cf_size, self.cf_size),
+            nn.BatchNorm1d(self.cf_size)
         )
         
         self.fc = nn.Sequential(
