f447d3d58ccbfec235e4f746f7d72a73b690b54b
Konrad Witkowski <31163991+KonWski@users.noreply.github.com>
2024-08-03 11:50:31 +0200

    Update CrossSiameseNet.py

--- CrossSiameseNet/CrossSiameseNet.py
+++ CrossSiameseNet/CrossSiameseNet.py
@@ -77,7 +77,19 @@ class CrossSiameseNet(nn.Module):
             nn.BatchNorm1d(self.cf_size*self.n_models),
             nn.Dropout(p=0.2, inplace=True)
                 )
+        self.features = nn.Sequential(
+            nn.Conv1d(self.n_models, 1, 1),
+            nn.ReLU(inplace=True),
+            nn.Flatten(start_dim=1),
+            nn.BatchNorm1d(2*self.cf_size),
+            nn.Dropout(p=0.2, inplace=True),
+            nn.Linear(2*self.cf_size, 2*self.cf_size),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm1d(2*self.cf_size),
+            nn.Dropout(p=0.2, inplace=True)
+        ) 
 
+        
         self.fc = nn.Sequential(
             nn.Linear(2*self.cf_size, 1),
             nn.Sigmoid()
