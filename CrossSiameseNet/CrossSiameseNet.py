b94638c0048945818c8ee8751c7f1e7266e50c1e
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2024-08-06 22:45:43 +0200

    csn residual connection

--- CrossSiameseNet/CrossSiameseNet.py
+++ CrossSiameseNet/CrossSiameseNet.py
@@ -11,6 +11,47 @@ from CrossSiameseNet.SiameseMolNet import SiameseMolNet
 from CrossSiameseNet.SiameseMolNetTriplet import SiameseMolNetTriplet
 
 
+class ConvBlock(nn.Module):
+
+    def __init__(self, dim_in: int, dim_out: int):
+
+        super().__init__()
+        self.conv = nn.Conv1d(dim_in, dim_out, 1)
+        self.activation_function = nn.ReLU(inplace=True)
+        self.batch_norm = nn.BatchNorm1d(dim_out)
+        self.dropout = nn.Dropout(p=0.2, inplace=True)
+
+    def forward(self, x):
+
+        x = self.conv(x)
+        x = self.activation_function(x)
+        x = self.batch_norm(x)
+        x = self.dropout(x)
+        return x
+
+
+class LinearBlock(nn.Module):
+
+    def __init__(self, dim_in: int, dim_out: int):
+
+        super().__init__()
+        self.linear = nn.Linear(dim_in, dim_out)
+        self.activation_function = nn.ReLU(inplace=True)
+        self.batch_norm = nn.BatchNorm1d(dim_out)
+        self.dropout = nn.Dropout(p=0.2, inplace=True)
+
+    def forward(self, x, residual = None):
+
+        if residual:
+            x += residual
+
+        x = self.linear(x)
+        x = self.activation_function(x)
+        x = self.batch_norm(x)
+        x = self.dropout(x)
+        return x
+    
+
 class CrossSiameseNet(nn.Module):
     '''Siamese network using features from other siamese networks'''
 
@@ -22,139 +63,13 @@ class CrossSiameseNet(nn.Module):
         self.n_models = len(models)
         self.cf_size = models[0].cf_size
 
-        self.features = nn.Sequential(
-            nn.Conv1d(self.n_models, 1, 1),
-            nn.ReLU(inplace=True),
-            nn.Flatten(start_dim=1),
-            nn.BatchNorm1d(2*self.cf_size),
-            nn.Dropout(p=0.2, inplace=True),
-            nn.Linear(2*self.cf_size, self.cf_size),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(self.cf_size),
-            nn.Dropout(p=0.2, inplace=True)
-        ) 
+        self.conv_block1 = ConvBlock(self.n_models, 64)
+        self.conv_block2 = ConvBlock(64, 128)
+        self.conv_block3 = ConvBlock(128, 128)
+        self.conv_block4 = ConvBlock(128, 64)
+        self.conv_block5 = ConvBlock(64, 1)
 
-        self.features = nn.Sequential(
-            nn.Conv1d(self.n_models, 2*self.n_models, 1),
-            nn.ReLU(inplace=True),
-            nn.Flatten(start_dim=1),
-            nn.BatchNorm1d(2*self.cf_size * 2 * self.n_models),
-            nn.Dropout(p=0.2, inplace=True),
-            nn.Linear(2*self.cf_size * 2 * self.n_models, self.cf_size * self.n_models),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(self.cf_size * self.n_models),
-            nn.Dropout(p=0.2, inplace=True)
-        ) 
-
-        self.features = nn.Sequential(
-            nn.Conv1d(self.n_models, self.n_models, 1),
-            nn.ReLU(inplace=True),
-            nn.Flatten(start_dim=1),
-            nn.BatchNorm1d(2*self.cf_size * self.n_models),
-            nn.Dropout(p=0.2, inplace=True),
-            nn.Linear(2*self.cf_size * self.n_models, 2* self.cf_size),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(2* self.cf_size),
-            nn.Dropout(p=0.2, inplace=True)
-        )
-
-        self.features = nn.Sequential(
-            nn.Conv1d(self.n_models, 1, 1),
-            nn.ReLU(inplace=True),
-            nn.Flatten(start_dim=1),
-            nn.BatchNorm1d(2*self.cf_size),
-            nn.Dropout(p=0.2, inplace=True)
-        ) 
-
-        self.features = nn.Sequential(
-            nn.Conv1d(self.n_models, self.n_models, 1),
-            nn.ReLU(inplace=True),
-            nn.Flatten(start_dim=1),
-            nn.BatchNorm1d(2*self.cf_size * self.n_models),
-            nn.Dropout(p=0.2, inplace=True),
-            nn.Linear(2*self.cf_size * self.n_models, self.cf_size * self.n_models),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(self.cf_size*self.n_models),
-            nn.Dropout(p=0.2, inplace=True)
-                )
-        self.features = nn.Sequential(
-            nn.Conv1d(self.n_models, 1, 1),
-            nn.ReLU(inplace=True),
-            nn.Flatten(start_dim=1),
-            nn.BatchNorm1d(2*self.cf_size),
-            nn.Dropout(p=0.2, inplace=True),
-            nn.Linear(2*self.cf_size, self.cf_size),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(self.cf_size),
-            nn.Dropout(p=0.2, inplace=True)
-        ) 
-
-        self.features = nn.Sequential(
-            nn.Conv1d(self.n_models, 32, 1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(32),
-            nn.Dropout(p=0.2, inplace=True),
-            nn.Conv1d(32, 1, 1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(1),
-            nn.Dropout(p=0.2, inplace=True),
-            nn.Flatten(start_dim=1)
-        )
-
-        self.features = nn.Sequential(
-            nn.Conv1d(self.n_models, 32, 1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(32),
-            nn.Dropout(p=0.2, inplace=True),
-            nn.Conv1d(32, 1, 1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(1),
-            nn.Dropout(p=0.2, inplace=True),
-            nn.Flatten(start_dim=1),
-
-            nn.Linear(2*self.cf_size, 2*self.cf_size),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(2*self.cf_size),
-            nn.Dropout(p=0.2, inplace=True)
-        )
-
-        self.features = nn.Sequential(
-            nn.Conv1d(self.n_models, 64, 1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(64),
-            nn.Dropout(p=0.2, inplace=True),
-
-            nn.Conv1d(64, 128, 1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(128),
-            nn.Dropout(p=0.2, inplace=True),
-
-            nn.Conv1d(128, 128, 1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(128),
-            nn.Dropout(p=0.2, inplace=True),
-
-            nn.Conv1d(128, 64, 1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(64),
-            nn.Dropout(p=0.2, inplace=True),
-
-            nn.Conv1d(64, 1, 1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(1),
-            nn.Dropout(p=0.2, inplace=True),
-            nn.Flatten(start_dim=1),
-            
-            nn.Linear(2*self.cf_size, 2*self.cf_size),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(2*self.cf_size),
-            nn.Dropout(p=0.2, inplace=True)
-        )
-
-        self.fc = nn.Sequential(
-            nn.Linear(2*self.cf_size, 1),
-            nn.Sigmoid()
-        ) 
+        self.linear_block = LinearBlock(2*self.cf_size, 2*self.cf_size)
 
         # turn off grads in all parameters 
         for model in self.models:
@@ -163,15 +78,12 @@ class CrossSiameseNet(nn.Module):
                 param.requires_grad = False
 
         # initialize the weights
-        for layer in self.fc:
-            if isinstance(layer, nn.Linear):
-                torch.nn.init.xavier_uniform_(layer.weight)
-                layer.bias.data.fill_(0.01)
-        
-        for layer in self.features:
-            if isinstance(layer, nn.Linear) or isinstance(layer, nn.Conv1d):
-                torch.nn.init.xavier_uniform_(layer.weight)
-                layer.bias.data.fill_(0.01)
+        for conv_block in [self.conv_block1, self.conv_block2, self.conv_block3, self.conv_block4, self.conv_block5]:
+            torch.nn.init.xavier_uniform_(conv_block.conv.weight)
+            conv_block.conv.bias.data.fill_(0.01)
+
+        torch.nn.init.xavier_uniform_(self.linear_block.linear.weight)
+        self.linear_block.linear.bias.data.fill_(0.01)
 
     def forward(self, x):
 
@@ -186,7 +98,15 @@ class CrossSiameseNet(nn.Module):
         # print(f"features_submodels.shape: {features_submodels.shape}")
 
         # print(f"features_submodels.shape: {features_submodels.shape}")
-        features = self.features(features_submodels)
+        x = self.conv_block1(features_submodels)
+        residual_features = residual_features
+        x = self.conv_block2(x)
+        x = self.conv_block3(x)
+        x = self.conv_block4(x)
+        x = self.conv_block5(x, residual_features)
+        x = self.linear_block(x)
+        
+        # features = self.features(features_submodels)
         # print(f"features.shape: {features.shape}")
         
-        return features
+        return x
