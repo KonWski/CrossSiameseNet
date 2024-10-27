7d304ba611315b7a749e64dff162c1e6879b1c4d
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2024-10-27 17:05:03 +0100

    load checkpoint update

--- CrossSiameseNet/checkpoints.py
+++ CrossSiameseNet/checkpoints.py
@@ -16,7 +16,47 @@ def save_checkpoint(checkpoint: dict, checkpoint_path: str):
     logging.info(8*"-")
 
 
-def load_checkpoint(checkpoint_path: str, triple_loss: bool = False):
+
+def load_checkpoint(model, checkpoint_path: str):
+    '''
+    loads model checkpoint from given path
+
+    Parameters
+    ----------
+    model
+        loaded type of model
+
+    Notes
+    -----
+    checkpoint: dict
+                parameters retrieved from training process i.e.:
+                - model_state_dict
+                - last finished number of epoch
+                - save time
+                - loss from last epoch testing
+                
+    '''
+    checkpoint = torch.load(checkpoint_path)
+    cf_size = 2048
+
+    # load parameters from checkpoint
+    model.load_state_dict(checkpoint["model_state_dict"])
+
+    # print loaded parameters
+    logging.info(f"Loaded model from checkpoint: {checkpoint_path}")
+    logging.info(f"Dataset: {checkpoint['dataset']}")    
+    logging.info(f"Epoch: {checkpoint['epoch']}")
+    logging.info(f"Used fixed training triplets: {checkpoint.get('used_fixed_training_triplets')}")
+    logging.info(f"Save dttm: {checkpoint['save_dttm']}")
+    logging.info(f"Train loss: {checkpoint['train_loss']}")    
+    logging.info(f"Test loss: {checkpoint['test_loss']}")
+
+    logging.info(8*"-")
+
+    return model, checkpoint
+
+
+def load_checkpoint_old(checkpoint_path: str, triple_loss: bool = False):
     '''
     loads model checkpoint from given path
 
