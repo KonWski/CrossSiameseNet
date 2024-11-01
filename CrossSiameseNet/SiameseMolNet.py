15f3b966daf18dc7d9184ecea44204ecc726c2e2
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2024-11-01 17:21:54 +0100

    SiameseMolNet

--- CrossSiameseNet/SiameseMolNet.py
+++ CrossSiameseNet/SiameseMolNet.py
@@ -3,15 +3,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class SiameseMolNet(nn.Module):
-    """
-    Siamese Network measuring the similarity between two molecules
-    """
+
     def __init__(self, cf_size: int, task: str):
 
         super().__init__()
 
         self.cf_size = cf_size
-        self.task = task
         self.linear_1 = nn.Linear(cf_size, 2*cf_size)
         self.batch_norm_1 = nn.BatchNorm1d(2*cf_size)
 
@@ -21,23 +18,17 @@ class SiameseMolNet(nn.Module):
         self.linear_3 = nn.Linear(2*cf_size, 2*cf_size)
         self.batch_norm_3 = nn.BatchNorm1d(2*cf_size)
 
-        self.linear_output_1 = nn.Linear(2*self.cf_size, self.cf_size)
-        self.batch_norm_6 = nn.BatchNorm1d(self.cf_size)
-
-        self.linear_output_2 = nn.Linear(self.cf_size, 64)
-        self.batch_norm_7 = nn.BatchNorm1d(64)
-
         if self.task == "classification":
-                self.linear_output_3 = nn.Linear(64, 2)
+                self.linear_output = nn.Linear(64, 2)
         elif self.task == "regression":
-                self.linear_output_3 = nn.Linear(64, 1)
+                self.linear_output = nn.Linear(64, 1)
 
         # initialize the weights
-        for layer in [self.linear_1, self.linear_2, self.linear_3, 
-                      self.linear_output_1, self.linear_output_2, self.linear_output_3]:
+        for layer in [self.linear_1, self.linear_2, self.linear_3, self.linear_output]:
             
             torch.nn.init.xavier_uniform_(layer.weight)
             layer.bias.data.fill_(0.01)
+
 
     def forward_once(self, x):
 
@@ -52,23 +43,10 @@ class SiameseMolNet(nn.Module):
 
         return features
 
-    def forward(self, mol0, mol1):
 
-        # process two molecules
-        features0 = self.forward_once(mol0)
-        features1 = self.forward_once(mol1)
+    def forward(self, x):
 
-        # combine both feature vectors
-        features = torch.stack((features0, features1), 0)
-        features_mean = torch.mean(features, 0)
-
-        # final output
-        output = F.relu(self.linear_output_1(features_mean))
-        output = self.batch_norm_6(output)
-
-        output = F.relu(self.linear_output_2(output))
-        output = self.batch_norm_7(output)
-        
-        output = self.linear_output_3(output)
+        features = self.forward_once(x)
+        output = self.linear_output(features)
 
         return output
