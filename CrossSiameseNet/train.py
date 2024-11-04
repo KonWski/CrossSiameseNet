d9729a2449ab4db90675222a482c2367a3735b59
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2024-11-04 18:00:40 +0100

    debug

--- CrossSiameseNet/train.py
+++ CrossSiameseNet/train.py
@@ -47,6 +47,7 @@ def train_triplet(model, dataset_name: str, train_loader: DataLoader, test_loade
                     optimizer.zero_grad()
 
                     if not already_transformed_mfs[0].item():
+                        print("Omitting model transformation")
                         anchor_mf = model(anchor_mf)
                         positive_mf = model(positive_mf)
                         negative_mf = model(negative_mf)
