82c64717e3999b5d97ebeeb7b9ab18d87d96702d
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2024-11-08 23:14:05 +0100

    semi_hard_negative_mining

--- CrossSiameseNet/BatchShaper.py
+++ CrossSiameseNet/BatchShaper.py
@@ -147,7 +147,7 @@ class BatchShaper:
                 positive_mfs_transformed = torch.stack(positive_mfs_transformed, dim=0)
                 negative_mfs_transformed = torch.stack(negative_mfs_transformed, dim=0)
 
-                if len(n_anchors_switched_to_hard_batch_1) > 0 or len(n_anchors_switched_to_hard_batch_0) > 0:
+                if n_anchors_switched_to_hard_batch_1 > 0 or n_anchors_switched_to_hard_batch_0 > 0:
                     logging.info(f"BatchShaper | n_anchors_switched_to_hard_batch_1: {n_anchors_switched_to_hard_batch_1}")
                     logging.info(f"BatchShaper | n_anchors_switched_to_hard_batch_0: {n_anchors_switched_to_hard_batch_0}")
 
