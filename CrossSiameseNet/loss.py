6bf0c2ce6b0a3a156f4fd31da9cbe4bd7641d906
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2024-11-10 00:11:50 +0100

    loss

--- CrossSiameseNet/loss.py
+++ CrossSiameseNet/loss.py
@@ -3,12 +3,15 @@ import torch
 
 class WeightedTripletMarginLoss(nn.Module):
     
-    def __init__(self, weights_1: float, margin: float = 1, reduction_type: str = "mean"):
+    def __init__(self, device, batch_size: int, weights_1: float, margin: float = 1, reduction_type: str = "mean"):
         super(WeightedTripletMarginLoss, self).__init__()
+        self.device = device
         self.weights_1 = weights_1
         self.margin = margin
         self.reduction_type = reduction_type
         self.distance_metric = nn.PairwiseDistance()
+        self.batch_size = batch_size
+        self._tensor_zero = torch.zeros(self.batch_size).to(self.device)
     
     def forward(self, anchor_mf, positive_mf, negative_mf, anchor_label):
 
@@ -16,8 +19,7 @@ class WeightedTripletMarginLoss(nn.Module):
         dist_anch_neg = self.distance_metric(anchor_mf, negative_mf)
         weights = torch.tensor([self.weights_1 for i in range(len(anchor_mf))]) * anchor_label
 
-        loss = torch.max(dist_anch_pos - dist_anch_neg + self.margin, torch.zeros(len(anchor_mf)))
-        loss = loss * weights
+        loss = torch.max(dist_anch_pos - dist_anch_neg + self.margin, self._tensor_zero) * weights
 
         if self.reduction_type == "mean":
             loss = loss.mean()
