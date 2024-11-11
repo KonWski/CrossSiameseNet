f8f24d37984b544697ca5e667121b53c12a1731f
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2024-11-11 22:18:48 +0100

    extra logs

--- CrossSiameseNet/BatchShaper.py
+++ CrossSiameseNet/BatchShaper.py
@@ -176,12 +176,12 @@ class BatchShaper:
                 indices_combined.append([i0, i1])
 
         distances_1_1 = distances[indices_1, indices_1]
-        distances_1_1_mean = round(torch.mean(distances_1_1).item(), 5)
+        distances_1_1_mean = round(torch.mean(distances_1_1[distances_1_1 != 0]).item(), 5)
 
         distances_0_0 = distances[indices_0, indices_0]
-        distances_0_0_mean = round(torch.mean(distances_0_0).item(), 5)
+        distances_0_0_mean = round(torch.mean(distances_0_0[distances_0_0 != 0]).item(), 5)
 
         distances_0_1 = distances[indices_combined]
-        distances_0_1_mean = round(torch.mean(distances_0_1).item(), 5)
+        distances_0_1_mean = round(torch.mean(distances_0_1[distances_0_1 != 0]).item(), 5)
 
         return distances_1_1_mean, distances_0_0_mean, distances_0_1_mean
