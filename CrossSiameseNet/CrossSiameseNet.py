87f3817bfb54ceac80ea8b3fc8d0d224e77d92ec
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2024-11-16 16:39:20 +0100

    alternative csn

--- CrossSiameseNet/CrossSiameseNet.py
+++ CrossSiameseNet/CrossSiameseNet.py
@@ -23,19 +23,104 @@ class ConvBlock(nn.Module):
         return x
 
 
+# class LinearBlock(nn.Module):
+
+#     def __init__(self, dim_in: int, dim_out: int):
+
+#         super().__init__()
+#         self.flatten = nn.Flatten(start_dim=1)
+#         self.linear = nn.Linear(dim_in, dim_out)
+#         self.activation_function = nn.ReLU(inplace=True)
+#         self.batch_norm = nn.BatchNorm1d(dim_out)
+
+#     def forward(self, x):
+        
+#         x = self.flatten(x)
+#         x = self.linear(x)
+#         x = self.activation_function(x)
+#         x = self.batch_norm(x)
+
+#         return x
+    
+
+# class CrossSiameseNet(nn.Module):
+#     '''Siamese network using features from other siamese networks'''
+
+#     def __init__(self, models: List[nn.Module]):
+
+#         super().__init__()
+
+#         self.models = models
+#         self.n_models = len(models)
+#         self.cf_size = models[0].cf_size
+
+#         self.conv_block1 = ConvBlock(self.n_models, 64)
+#         self.conv_block2 = ConvBlock(64, 64)
+#         self.conv_block3 = ConvBlock(64, 64)
+#         self.conv_block4 = ConvBlock(64, 64)
+#         self.conv_block5 = ConvBlock(64, 64)
+#         self.conv_block6 = ConvBlock(64, 2)
+
+#         self.linear_block = LinearBlock(4*self.cf_size, 2*self.cf_size)
+
+#         # turn off grads in all parameters 
+#         for model in self.models:
+#             model.eval()
+#             for param in model.parameters():
+#                 param.requires_grad = False
+
+#         # initialize the weights
+#         for conv_block in [self.conv_block1, self.conv_block2, self.conv_block3, 
+#                            self.conv_block4, self.conv_block5, self.conv_block6]:
+#             torch.nn.init.xavier_uniform_(conv_block.conv.weight)
+#             conv_block.conv.bias.data.fill_(0.01)
+
+#         torch.nn.init.xavier_uniform_(self.linear_block.linear.weight)
+#         self.linear_block.linear.bias.data.fill_(0.01)
+
+#     def forward_once(self, x):
+
+#         # features collected across all models
+#         features_submodels = [model.forward_once(x) for model in self.models]
+#         print(f"features_submodels[0].shape: {features_submodels[0].shape}")
+#         print(f"features_submodels[1].shape: {features_submodels[1].shape}")
+
+#         features_submodels = torch.stack(features_submodels, dim=-2)
+#         print(f"features_submodels.shape: {features_submodels.shape}")
+
+#         x = self.conv_block1(features_submodels)
+#         residual_features0 = x
+
+#         x = self.conv_block2(x)
+#         x = self.conv_block3(x, residual_features0)
+#         residual_features1 = x
+        
+#         x = self.conv_block4(x)
+#         x = self.conv_block5(x, residual_features1)
+#         x = self.conv_block6(x)
+#         x = self.linear_block(x)
+
+#         return x
+
+#     def forward(self, x):
+#         return self.forward_once(x)
+    
+
+# alternative version
 class LinearBlock(nn.Module):
 
     def __init__(self, dim_in: int, dim_out: int):
 
         super().__init__()
-        self.flatten = nn.Flatten(start_dim=1)
         self.linear = nn.Linear(dim_in, dim_out)
         self.activation_function = nn.ReLU(inplace=True)
         self.batch_norm = nn.BatchNorm1d(dim_out)
 
-    def forward(self, x):
+    def forward(self, x, residual = None):
+
+        if residual is not None:
+            x += residual
         
-        x = self.flatten(x)
         x = self.linear(x)
         x = self.activation_function(x)
         x = self.batch_norm(x)
@@ -54,14 +139,11 @@ class CrossSiameseNet(nn.Module):
         self.n_models = len(models)
         self.cf_size = models[0].cf_size
 
-        self.conv_block1 = ConvBlock(self.n_models, 64)
-        self.conv_block2 = ConvBlock(64, 64)
-        self.conv_block3 = ConvBlock(64, 64)
-        self.conv_block4 = ConvBlock(64, 64)
-        self.conv_block5 = ConvBlock(64, 64)
-        self.conv_block6 = ConvBlock(64, 2)
-
-        self.linear_block = LinearBlock(4*self.cf_size, 2*self.cf_size)
+        self.conv_block1 = ConvBlock(self.n_models, 2)
+        self.flatten = nn.Flatten(start_dim=1)
+        self.linear_block1 = LinearBlock(4*self.cf_size, 4*self.cf_size)
+        self.linear_block2 = LinearBlock(4*self.cf_size, 4*self.cf_size)
+        self.linear_block3 = LinearBlock(4*self.cf_size, 4*self.cf_size)
 
         # turn off grads in all parameters 
         for model in self.models:
@@ -69,36 +151,28 @@ class CrossSiameseNet(nn.Module):
             for param in model.parameters():
                 param.requires_grad = False
 
-        # initialize the weights
-        for conv_block in [self.conv_block1, self.conv_block2, self.conv_block3, 
-                           self.conv_block4, self.conv_block5, self.conv_block6]:
+        # initialize the weights        
+        for conv_block in [self.conv_block1]:
             torch.nn.init.xavier_uniform_(conv_block.conv.weight)
             conv_block.conv.bias.data.fill_(0.01)
 
-        torch.nn.init.xavier_uniform_(self.linear_block.linear.weight)
-        self.linear_block.linear.bias.data.fill_(0.01)
+        for lin_block in [self.linear_block1, self.linear_block2, self.linear_block3]:
+            torch.nn.init.xavier_uniform_(lin_block.linear.weight)
+            lin_block.linear.bias.data.fill_(0.01)
 
     def forward_once(self, x):
 
         # features collected across all models
         features_submodels = [model.forward_once(x) for model in self.models]
-        # print(f"features_submodels[0].shape: {features_submodels[0].shape}")
-        # print(f"features_submodels[1].shape: {features_submodels[1].shape}")
-
         features_submodels = torch.stack(features_submodels, dim=-2)
-        # print(f"features_submodels.shape: {features_submodels.shape}")
 
         x = self.conv_block1(features_submodels)
-        residual_features0 = x
+        x = self.flatten(x)
+        residual_features = x.copy()
 
-        x = self.conv_block2(x)
-        x = self.conv_block3(x, residual_features0)
-        residual_features1 = x
-        
-        x = self.conv_block4(x)
-        x = self.conv_block5(x, residual_features1)
-        x = self.conv_block6(x)
-        x = self.linear_block(x)
+        x = self.linear_block1(x)
+        x = self.linear_block2(x, residual_features)
+        x = self.linear_block3(x)
 
         return x
 
